b6536b00dfb3859ce471d4ba60517c24a4ab55cf
IsabelNzioka <isabelnzioka@gmail.com>
2023-05-09 19:39:57 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const request = require("request");
+
+const key = "de207b443b0649d883d7621590a98347";
+const lat = "";
+const lon = "";
+const url = `api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${key}`;
